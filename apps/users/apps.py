e63333e8b4919bd85438e81096041ba140b77c21
efoedem <kwakuedemhlodzeprince@gmail.com>
2026-01-11 09:33:22 +0000

    Update admin credentials and fix apps logic

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+# This function runs after the database is ready
+def create_admin(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    # Check if 'admin' already exists so we don't create it twice
+    if not User.objects.filter(username="admin_edem").exists():
+        User.objects.create_superuser("admin_edem", "efoedem@example.com", "Titivate22@$")
+        print("Admin user admin_edem created successfully!")
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.users' # Important: add the 'apps.' prefix
+    name = 'apps.users'
+
+    def ready(self):
+        # We connect the function to the post_migrate signal
+        post_migrate.connect(create_admin, sender=self)
