1a815f55de0128c8d34acc2de3c8505d4312109c
efoedem <kwakuedemhlodzeprince@gmail.com>
2026-01-11 13:01:43 +0000

    Transfer local course data to live database

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
-# Clean old static files and collect fresh ones
 python manage.py collectstatic --no-input --clear
-
-# Apply migrations to the new PostgreSQL database
 python manage.py migrate
 
-# Create a superuser if variables are provided (Essential for Free Tier)
+# Add this line to import your courses
+if [ -f data.json ]; then
+    python manage.py loaddata data.json
+fi
+
 if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --no-input || true
 fi
