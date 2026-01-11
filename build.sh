2535250437b4d63c36c6c1f3f3f1e287990e5a70
efoedem <kwakuedemhlodzeprince@gmail.com>
2026-01-11 13:20:50 +0000

    Cleanup: removed data.json and loaddata command

--- build.sh
+++ build.sh
@@ -5,10 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
 
-# Add this line to import your courses
-if [ -f data.json ]; then
-    python manage.py loaddata data.json
-fi
+
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --no-input || true
